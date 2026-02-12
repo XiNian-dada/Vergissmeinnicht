01fb8599d2278c86c93ae219fb6cc64b5ef77500
XiNian-dada <2188349576@qq.com>
2026-02-13 04:12:56 +0800

    chores: 修复了两个标题连用时不换行的BUG，修复了1.3.0分类机制造成的500Err

--- template-parts/category-sub.php
+++ template-parts/category-sub.php
@@ -1,7 +1,9 @@
 <?php use widget\CategorySub; if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
-<?php if($this->pageRow['mid']): ?>
-    <?php CategorySub::alloc(['parent' => $this->pageRow['mid']])->to($categories) ?>
+<?php if($this->mid): ?>
+    
+    <?php CategorySub::alloc(['parent' => $this->mid])->to($categories) ?>
+    
     <?php if ($categories->have()): ?>
         <div class="sub-category-nav">
             <a class="cat-pill active" href="<?php $this->archiveUrl(); ?>">全部</a>
